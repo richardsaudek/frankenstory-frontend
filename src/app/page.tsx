9a30f111178542dbb3f3a0aa2ff79b8ea027e11a
richardsaudek <richard.saudek@gmail.com>
2023-07-12 14:32:59 -0400

    file structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import 'styles/global.css' 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Page() {
+  return <h1 className="text-3xl font-bold underline">Frankenstory!</h1>
+}
+/*export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -121,4 +125,4 @@ export default function Home() {
       </div>
     </main>
   )
-}
+}*/
